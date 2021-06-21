1df076eefc0bd35ce059e48d9562f7c4f32bb9b6
BenToronto <83716543+BenToronto@users.noreply.github.com>
2021-06-21 12:26:55 -0400

    Create msbuild.cpp

---
+++ .github/workflows/msbuild.cpp
@@ -0,0 +1,86 @@
+std::push_heap(std::begin(container), std::end(container));
+auto pos = std::partition_point(std::begin(container), std::end(container), []() {
+  
+});
+if (pos != std::end(container)) {
+  
+}
+
+
+if (std::lexigraphical_compare(std::begin(container), std::end(container),
+  std::begin(), std::end()) {
+  
+}
+
+auto pos = std::adjacent_find(std::begin(container), std::end(container));
+if (pos != std::end(container)) {
+  
+}
+
+
+line;
+
+container.erase(0, container.find_first_not_of(" \t\n\r"));std::transform(std::begin(container), std::end(container), std::begin(container), [](char c) {
+return std::tolower(c); });\
+
+std::transform(std::begin(container), std::end(container),
+  std::begin(container), []() {
+
+});
+
+if (std::any_of(std::begin(container), std::end(container), []() {
+  
+})) {
+  
+}
+
+
+using y.z.xor
+
+__interface IInterface
+{
+    
+};
+
+std::partition_copy(std::begin(container), std::end(container),
+                  std::begin(), std::end());
+
+
+
+
+                  namespace std;
+                  {
+                      
+                  }
+
+                  return 0;
+
+
+                  auto pos = std::set_difference(std::begin(container), std::end(container),
+                    std::begin(), std::end(), std::begin());
+
+
+                    auto pos = std::lower_bound(std::begin(container), std::end(container), );
+                    if (pos != std::end(container)) {
+                      
+                    }
+
+                    if (std::lexigraphical_compare(std::begin(container), std::end(container),
+                      std::begin(), std::end()) {
+                      
+                    }
+                    if (std::lexigraphical_compare(std::begin(container), std::end(container),
+                      std::begin(), std::end()) {
+                      
+                    }
+
+
+
+                    if (y,x)
+                    {
+                        
+                    }
+
+                    if (std::is_sorted(std::begin(container), std::end(container))) {
+                      
+                    }
